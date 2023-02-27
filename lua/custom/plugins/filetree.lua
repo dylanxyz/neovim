2adaf08821adf53aea9b15681dd028b66a6ab74a
Jorge Brito <jorge.brito.xyz@gmail.com>
2023-02-27 13:16:30 -0300

    First commit 🦠

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,55 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    {
+        -- only needed if you want to use the commands with "_with_window_picker" suffix
+        's1n7ax/nvim-window-picker',
+        version = "*",
+        config = function()
+          require'window-picker'.setup({
+            autoselect_one = true,
+            include_current = false,
+            filter_rules = {
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { 'terminal', "quickfix" },
+              },
+            },
+            other_win_hl_color = '#e35e4f',
+          })
+        end,
+      }
+  },
+  config = function ()
+    -- Unless you are still migrating, remove the deprecated commands from v1.x
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+    
+    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+    vim.fn.sign_define("DiagnosticSignError",
+      {text = " ", texthl = "DiagnosticSignError"})
+    vim.fn.sign_define("DiagnosticSignWarn",
+      {text = " ", texthl = "DiagnosticSignWarn"})
+    vim.fn.sign_define("DiagnosticSignInfo",
+      {text = " ", texthl = "DiagnosticSignInfo"})
+    vim.fn.sign_define("DiagnosticSignHint",
+      {text = "", texthl = "DiagnosticSignHint"})
+
+    require('neo-tree').setup {
+      window = {
+        width = 30,
+      }
+    }
+    
+    vim.cmd [[ nnoremap <leader>f :Neotree reveal<cr> ]]
+    vim.cmd [[ nnoremap <leader>b :Neotree toggle show buffers right<cr> ]]
+    vim.cmd [[ nnoremap <C-b>     :Neotree toggle<cr> ]]
+  end,
+}
